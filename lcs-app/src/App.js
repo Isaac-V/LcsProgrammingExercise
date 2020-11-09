a360daa059a083acf39269ea9e7a751eead07be3
Isaac Vawter <isaac.vawter@gmail.com>
2020-11-09 18:47:53 +0100

    Starter Code

---
+++ lcs-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MemberList from "./MemberList";
+
+function App() {
+  return (
+    <div className="App"><div>
+      <h3>Members</h3>
+      <MemberList />
+    </div>
+    </div>
+  );
+}
+
+export default App;
